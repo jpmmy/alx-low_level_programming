e47920fbe21cb62fe1dba608377860ec8b665deb
jpmmy <jimyabdu@gmail.com>
2022-03-14 04:15:34 +0000

    add C file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after the printf command
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19with proper grammar, but the outcome is a piece of art,\n");
+
+	return (1);
+}
