8dc3219368cb4dcb59fc47321f29ea41c18ae405
jpmmy <jimyabdu@gmail.com>
2022-03-14 04:20:27 +0000

    add C file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19with proper grammar, but the outcome is a piece of art,\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
