18333f443018200e9c28ea10ec672321a7bcd5e3
jpmmy <jimyabdu@gmail.com>
2022-03-19 16:31:15 +0000

    add C file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * function prints single digit numbers combination
+ *
+ * Return: return 0 for main
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	j = '0';
+	k = ',';
+	l = ' ';
+
+ for (i = 0; i < 10; i++) 
+ {
+	    putchar(j);
+	       if (i < 9)
+	       {
+		       putchar(k);
+		       putchar(l);
+	       }
+	      j = j + 1;
+ }
+ putchar('\n');
+ return 0;
+}
