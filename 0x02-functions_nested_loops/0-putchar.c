357bdb65ff85618c4aee3b44aff87c95481818c1
jpmmy <jimyabdu@gmail.com>
2022-03-21 07:09:52 +0000

    add Files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * main - entry point
+ *
+ * print words
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int leng;
+	char txt[] = "_putchar";
+
+	leng = sizeof(txt) / sizeof(txt[0]);
+	for (int i = 0; i < leng; i++)
+	{
+		if (txt[i] != '\0')
+		{
+			_putchar(txt[i]);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+README File for 0x02-functions_nested_loops project
