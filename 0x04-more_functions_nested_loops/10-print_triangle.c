d7f04847e3133167f82767152dd3f62ce5d09e07
jpmmy <jimyabdu@gmail.com>
2022-03-27 04:16:11 +0000

    add function that prints square

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints a square, followed by a new line
+ * @size: size of triangle
+ * Return: Returs nothing.
+ */
+
+void print_triangle(int size)
+{
+	int  i, j, x;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				x = (size - i) - 1;
+				if (j < x)
+					_putchar(' ');
+				else
+					_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
