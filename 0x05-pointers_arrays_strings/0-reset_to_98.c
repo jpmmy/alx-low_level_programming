d950925c547ea493f1d0b57b962df4aa993be55b
jpmmy <jimyabdu@gmail.com>
2022-03-27 04:44:08 +0000

    initial commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
+ * @n:number to be replaced
  *
  * Return: return nothing
  */
