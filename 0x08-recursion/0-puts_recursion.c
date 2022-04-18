00dda51179fa0aa33519d58f53c2515a74cb9aa2
jpmmy <jimyabdu@gmail.com>
2022-04-18 07:43:52 +0000

    add puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,15 +10,16 @@
 
 void _puts_recursion(char *s)
 {
-		int i;
 
-		i = 0;
-		while (s[i] != '\0')
+		if (*s != '\0')
 		{
-		_putchar(s[i]);
-		i++;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 		}
+		else
+		{
 		_putchar('\n');
+		}
 }
 
 
