b3be5c35fc7430eae270dace00c74406abee74d3
jpmmy <jimyabdu@gmail.com>
2022-04-19 11:20:03 +0000

    add function to check if n is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * is_prime_number - a function that returns 1 if n is prime number
+ * and return 0 if it is not.
+ * @n:param n
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+	{
+		return (is_prime(n, 2));
+	}
+/**
+ * is_prime - a helper function that returns 1 if n is prime number
+ * and return 0 if it is not.
+ * @n:param n
+ * @i:param i
+ * Return: 1 or 0
+ */
+int is_prime(int n, int i)
+{
+	if (n == 0 || n == 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+	}
+	return (is_prime(n, i + 1));
+}
