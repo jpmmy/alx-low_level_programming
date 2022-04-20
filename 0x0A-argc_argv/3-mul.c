9387a8a3e2fcfad0c3326dc11101e1a94cd71cc8
jpmmy <jimyabdu@gmail.com>
2022-04-20 12:52:26 +0000

    add program that prints product of numbers passed to it

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - a program that prints product of two numbers
+ * @argc:param argc
+ * @argv:param argv
+ *
+ * Return:1 on error and 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, prod;
+
+	i = 1;
+	prod = 1;
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		while (i < argc)
+		{
+		prod = prod * atoi(argv[i]);
+		i++;
+		}
+		printf("%d\n", prod);
+	}
+	return (0);
+}
