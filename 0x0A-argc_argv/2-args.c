e399d7c9f84f4a170cb4fc1fc02794820916ef89
jpmmy <jimyabdu@gmail.com>
2022-04-20 12:28:41 +0000

    add program to print argv content

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - a program that prints the arguments passed to it
+ * @argc:param argc
+ * @argv:param argv
+ *
+ * Return:none
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
+	}
+	return (0);
+}
