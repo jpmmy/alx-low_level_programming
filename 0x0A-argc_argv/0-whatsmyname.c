0fb05106290208406369a19de645dd45560597cc
jpmmy <jimyabdu@gmail.com>
2022-04-20 11:48:50 +0000

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - a program that prints its name
+ * followed by a new line
+ * @argc:param argc
+ * @argv:param argv
+ *
+ * Return:none
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
