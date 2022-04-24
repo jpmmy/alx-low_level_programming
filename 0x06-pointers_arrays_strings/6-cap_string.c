e84f94498a3407f1609e8509b9cc29b0f80737d4
jpmmy <jimyabdu@gmail.com>
2022-04-24 14:15:52 +0000

    add a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,88 +10,27 @@
 char *cap_string(char *s)
 {
 
-	int i, l, j;
+	int i, j;
 
-	i = 0;
-	j = 0;
-	l = _strlen_recursion(s);
-	while (i < l && s[i] != '\0')
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (word_s(s[i]))
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
 		{
-			if (s[i] <= 'z' && s[i] >= 'a' && j == 1)
+			if (s[i] == spe[j])
 			{
-				s[i] = s[i] - 32;
-				j = 0;
-			}
-			else
-			{
-				j = 0;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
 			}
 		}
-		else
-		{
-			j = 1;
-		}
-		i++;
 	}
-	return (s);
-}
-/**
- * word_s - a helper function that checkes for word
- * separators.
- * @s:param s. a string to be checked.
- *
- *  Return: return 0 if character matches word separator and 1 if not
- */
-int word_s(char s)
-{
-switch (s)
-{
-case ',':
-	return (0);
-case ';':
-	return (0);
-case '.':
-	return (0);
-case '!':
-	return (0);
-case '?':
-	return (0);
-case '"':
-	return (0);
-case '(':
-	return (0);
-case ')':
-	return (0);
-case '{':
-	return (0);
-case '}':
-	return (0);
-case ' ':
-	return (0);
-case '\t':
-	return (0);
-case '\n':
-	return (0);
-default:
-	return (1);
-}
-}
-/**
- * _strlen_recursion - a function that return the length of a string
- * @s:string to be measured
- * Return: the length of the string
- */
-int _strlen_recursion(char *s)
-{
 
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
+return (s);
 }
