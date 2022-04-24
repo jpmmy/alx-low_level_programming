23024c6fcad54859303cb0c248c07bc1757c68ac
jpmmy <jimyabdu@gmail.com>
2022-04-24 12:02:24 +0000

    add a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,79 @@
+#include"main.h"
+
+/**
+ * cap_string- a function that
+ * capitalizes all words of a string.
+ * @s: string to be converted
+ *
+ * Return: return s (all capital)
+ */
+char *cap_string(char *s)
+{
+
+	int i, l;
+
+	i = 0;
+	l = _strlen_recursion(s);
+	while (i < l && s[i] != '\0')
+	{
+		if (word_s(s[i]))
+		{
+			if (s[i] <= 'z' && s[i] >= 'a')
+				s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
+/**
+ * word_s - a helper function that checkes for word
+ * separators.
+ * @s:param s. a string to be checked.
+ *
+ *  Return: return 0 if character matches word separator and 1 if not
+ */
+int word_s(char s)
+{
+switch (s)
+{
+case ',':
+	return (0);
+case ';':
+	return (0);
+case '.':
+	return (0);
+case '!':
+	return (0);
+case '?':
+	return (0);
+case '"':
+	return (0);
+case '(':
+	return (0);
+case ')':
+	return (0);
+case '{':
+	return (0);
+case '}':
+	return (0);
+default:
+	return (1);
+}
+}
+/**
+ * _strlen_recursion - a function that return the length of a string
+ * @s:string to be measured
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
