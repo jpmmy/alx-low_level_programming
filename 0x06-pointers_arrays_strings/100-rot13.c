6941e4b7befaaae55c9eef3c890ef2435a272e6d
jpmmy <jimyabdu@gmail.com>
2022-04-25 12:59:14 +0000

    add function to encode a string to Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,20 +16,14 @@ char *rot13(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		cp = s[i];
-		while ((cp >= 'a' && cp <= 'z') || (cp >= 'A' && cp <= 'Z'))
-		{
 			if ((cp > 'm' && cp <= 'z') || (cp > 'M' && cp <= 'Z'))
 			{
 				cp = cp - 13;
-				goto retur;
 			}
 			else if ((cp >= 'a' && cp < 'n') || (cp >= 'A' && cp < 'N'))
 			{
 				cp = cp + 13;
-				goto retur;
 			}
-		}
-retur:
 		s[i] = cp;
 	}
 
