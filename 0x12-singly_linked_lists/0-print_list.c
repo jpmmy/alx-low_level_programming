061c1c1c2be70b63089ac554ad0088c95ff7fc07
jpmmy <jimyabdu@gmail.com>
2022-04-26 11:33:56 +0000

    add a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
   *print_list - prints all the elements of a list_t.
   *@h: pointer to list.
   *
-  *Return: the number of nodes.
+  *Return:number of nodes.
   */
 size_t print_list(const list_t *h)
 {
@@ -25,3 +25,4 @@ size_t print_list(const list_t *h)
 
 	return (count);
 }
+
