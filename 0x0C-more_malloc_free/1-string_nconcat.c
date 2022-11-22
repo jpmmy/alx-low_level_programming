3c2c6a7889284a510caeeec812c1ea759bfeeebb
jpmmy <jimyabdu@gmail.com>
2022-11-22 06:43:08 +0000

    add function that concatenates strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,88 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - a function that concatenates two
+ * strings
+ * @s1:param 1
+ * @s2:param 2
+ * @n:param 3
+ * Return:a pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int l1, l2;
+	char *ret;
+
+	if (s1 == NULL)
+	{
+		l1 = 0;
+	}
+	else
+	{
+		l1 = _strlen(s1);
+	}
+	if (s2 == NULL)
+	{
+		l2 = 0;
+	}
+	else
+	{
+		l2 = _strlen(s2);
+	}
+	if (n >= l2)
+	{
+		ret = malloc(sizeof(char) * ((l1 + l2) + 1));
+	}
+	else
+	{
+		ret = malloc(sizeof(char) * ((l1 + n) + 1));
+	}
+	helper_conc(ret, s1, s2, n);
+	return (ret);
+}
+/**
+ * helper_conc - helper function for the concatenate function
+ * @ret: param 1
+ * @s1:param 2
+ * @s2:patam 3
+ * @n: param 4
+ * Return: pointer to the allocated space.
+ */
+char *helper_conc(char *ret, char *s1, char *s2, unsigned int n)
+{
+	unsigned int x, y;
+
+	x = 0;
+	y = 0;
+	while (s1[x] != '\0')
+	{
+		ret[x] = s1[x];
+		x++;
+	}
+	while (y <= n)
+	{
+		ret[x] = s2[y];
+		x++;
+		y++;
+	}
+	ret[x] = '\0';
+	return (ret);
+}
+/**
+ * _strlen - function that returns the l of a string
+ * @s: pointer to the char value
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i, l;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	l = i;
+	return (l);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _strlen(char *s);
+char *helper_conc(char *ret,char *s1,char *s2, unsigned int n);
 
 #endif /* MAIN_H */
