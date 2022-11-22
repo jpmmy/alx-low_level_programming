2c81115f7cf798fbaea7a185ed3a9e6a1f272756
jpmmy <jimyabdu@gmail.com>
2022-11-22 17:25:58 +0000

    add a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@
  * @name: param name
  * @age: param age
  * @owner: param owner
- * Return: pointer to the struct
+ * Return: pointer to the struct dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -15,8 +15,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	t = malloc(sizeof(dog_t));
 	if (t == NULL)
 		return (NULL);
-	t->name = name;
+	t->name = malloc(sizeof(dog_t));
+	if (t->name == NULL)
+		return (NULL);
 	t->age = age;
-	t->owner = owner;
+	t->owner = malloc(sizeof(dog_t));
+	_strcpy(t->name, name);
+	_strcpy(t->owner, owner);
+
 	return (t);
+}
+/**
+ * _strcpy - function that copies string.
+ * @dest: destination
+ * @src: string to be copied
+ * Return: Address of desc
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
